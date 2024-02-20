67a9acc48feb7998054de4944c68d6d952486da5
nikusingh1729 <niku.singh@trumio.ai>
2024-02-20 11:54:23 +0530

    upodated name and logo

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -30,7 +30,7 @@ export default function Landing() {
                 <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
 
                     <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                        RIGLabs <br /> is the New Trend.
+                        RIGVerse <br /> is the New Trend.
                     </h1>
                     <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
                         RIGLabs represents the forefront of gaming innovation, serving as a dynamic
