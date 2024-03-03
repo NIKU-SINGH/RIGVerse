d232c589f5e119e218dc122c3f94292c5553097f
NIKU-SINGH <soapmactavishmw4@gmail.com>
2024-03-03 20:07:55 +0530

    resolved merge conflict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,38 +10,38 @@ import { useCallback, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
-  const sendNotification = () => {
-    if ("Notification" in window && Notification.permission === "granted") {
-      new Notification("Hello Developers!", { body: "This is notification!" });
-    }
-  };
+        const sendNotification = () => {
+                if ("Notification" in window && Notification.permission === "granted") {
+                        new Notification("Hello Developers!", { body: "This is notification!" });
+                }
+        };
 
-  const requestNotificationPermission = useCallback(() => {
-    if ("Notification" in window) {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-          console.log("Notification permission granted!");
-          sendNotification();
-        }
-      });
-    }
-  }, []);
+        const requestNotificationPermission = useCallback(() => {
+                if ("Notification" in window) {
+                        Notification.requestPermission().then(function (permission) {
+                                if (permission === "granted") {
+                                        console.log("Notification permission granted!");
+                                        sendNotification();
+                                }
+                        });
+                }
+        }, []);
 
-  // useEffect(() => {
-  //   if ("Notification" in window) {
-  //   }
-  //   requestNotificationPermission();
-  // }, [requestNotificationPermission]);
+        // useEffect(() => {
+        //   if ("Notification" in window) {
+        //   }
+        //   requestNotificationPermission();
+        // }, [requestNotificationPermission]);
 
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between px-6 md:px-24 py-4 ${inter.className}`}
-    >
-      <Landing />
-      <button onClick={sendNotification}>Trigger notification</button>
-      {/* <Feature /> */}
-      {/* <BackgroundGradient /> */}
-      <CTA />
-    </main>
-  );
+        return (
+                <main
+                        className={`flex flex-col items-center justify-between px-6 md:px-24 py-24 ${inter.className}`}
+                >
+                        <Landing />
+                        <button onClick={sendNotification}>Trigger notification</button>
+                        {/* <Feature /> */}
+                        {/* <BackgroundGradient /> */}
+                        {/* <CTA /> */}
+                </main>
+        );
 }
