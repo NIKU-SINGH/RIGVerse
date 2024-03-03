13d96f7f956677c097e8a08658d22a46b26a3c55
NIKU-SINGH <soapmactavishmw4@gmail.com>
2024-03-04 01:58:01 +0530

    feat(ui): Added the game page

--- src/components/Cards/ProductCard/index.tsx
+++ src/components/Cards/ProductCard/index.tsx
@@ -4,61 +4,70 @@ import { Button } from '@/components/ui/button'
 import { Tag } from 'lucide-react';
 import { OnSaleBadge, PurchasedBadge, RequireGamePassBadge } from '@/components/Badges';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 interface GameProps {
-    game: {
-        id: number;
-        name: string;
-        price: number;
-        image: string;
-        category: string[];
-        viewerCount: string;
-        badge: string;
-    },
-    className?: string;
-    endpoint?: string;
+        game: {
+                id: number;
+                name: string;
+                price: number;
+                image: string;
+                category: string[];
+                viewerCount: string;
+                badge: string;
+        },
+        className?: string;
+        endpoint?: string;
 
 }
 const Index: React.FC<GameProps> = ({ game, className, endpoint = "0xsdhj24nghj" }) => {
-    return (
-        <>
-            <Link href={endpoint + "/" + game.id}>
+        const router = useRouter();
 
-                <div className={'${className} relative border-2 border-gray-800 cursor-pointer hover:bg-gray-800  rounded-2xl group overflow-hidden  p-4'}>
-                    <div className="relative w-full h-48">
-                        <Image
-                            src={game.image}
-                            alt="feature"
-                            layout="fill"
-                            objectFit="cover"
-                            className="rounded-2xl"
-                        />
-                    </div>
-                    <div className='flex flex-col px-2 py-2 w-full rounded-lg gap-2'>
-                        {/* Category tag with tooltip */}
-                        <div className="flex gap-1 flex-wrap">
-                            {game.category && game?.category.map((category, index) => (
-                                <span key={index} className="bg-blue-600 text-white px-2 rounded-lg text-xs">{category}</span>
-                            ))}
+        const handleRedirect = () => {
+                router.push({
+                        pathname: endpoint + "/" + game.id,
+                        query: { game: JSON.stringify(game) },
+                });
+        };
+        return (
+                <>
+                        <div onClick={handleRedirect}>
+
+                                <div className={'${className} relative border-2 border-gray-800 cursor-pointer hover:bg-gray-800  rounded-2xl group overflow-hidden  p-4'}>
+                                        <div className="relative w-full h-48">
+                                                <Image
+                                                        src={game.image}
+                                                        alt="feature"
+                                                        layout="fill"
+                                                        objectFit="cover"
+                                                        className="rounded-2xl"
+                                                />
+                                        </div>
+                                        <div className='flex flex-col px-2 py-2 w-full rounded-lg gap-2'>
+                                                {/* Category tag with tooltip */}
+                                                <div className="flex gap-1 flex-wrap">
+                                                        {game.category && game?.category.map((category, index) => (
+                                                                <span key={index} className="bg-blue-600 text-white px-2 rounded-lg text-xs">{category}</span>
+                                                        ))}
+                                                </div>
+                                                {/* Game title and viewer count */}
+                                                <div className="">
+                                                        <span className="text-white bg-red-600 px-2 rounded-full text-xs">4.9K Active Gamers</span>
+                                                        <p className="text-gray-300 text-sm">{game?.price}+ Active memebers</p>
+                                                </div>
+
+                                                <h1 className="text-white text-md font-medium  tracking-tight ">
+                                                        {game.name}
+                                                </h1>
+                                                {/* Conditionally display badges */}
+                                                {game.badge === "OnSale" && <OnSaleBadge />}
+                                                {game.badge === "Purchased" && <PurchasedBadge />}
+                                                {game.badge === "RequireGamePass" && <RequireGamePassBadge />}
+                                        </div>
+                                </div >
                         </div>
-                        {/* Game title and viewer count */}
-                        <div className="">
-                            <span className="text-white bg-red-600 px-2 rounded-full text-xs">4.9K Active Gamers</span>
-                            <p className="text-gray-300 text-sm">{game?.price}+ Active memebers</p>
-                        </div>
+                </>
 
-                        <h1 className="text-white text-md font-medium  tracking-tight ">
-                            {game.name}
-                        </h1>
-                        {/* Conditionally display badges */}
-                        {game.badge === "OnSale" && <OnSaleBadge />}
-                        {game.badge === "Purchased" && <PurchasedBadge />}
-                        {game.badge === "RequireGamePass" && <RequireGamePassBadge />}
-                    </div>
-                </div >
-            </Link>
-        </>
-
-    )
+        )
 }
 
 export default Index
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -56,7 +56,7 @@ function UserSettings() {
                                 </div>
 
                                 {/* Profile image upload */}
-                                <div className="flex gap-4">
+                                <div className="flex gap-4 text-gray-200">
                                         <div className="my-4 p-4 rounded-2xl bg-gray-800 w-96">
                                                 {/* <h2 className="text-lg font-semibold">Profile Image</h2> */}
                                                 <div className="flex gap-2 flex-col items-center justify-center">
@@ -165,7 +165,7 @@ function UserSettings() {
                                 <div className="bg-gray-800 my-4 p-8 rounded-lg shadow-lg max-w-md w-full">
                                         <h2 className="text-2xl font-bold mb-6 text-gray-200">Social Accounts</h2>
 
-                                        <div className="space-y-4">
+                                        <div className="space-y-4 text-gray-200">
                                                 <div className="flex justify-between items-center py-2">
                                                         <div className="flex items-center">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
